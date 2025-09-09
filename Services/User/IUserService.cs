20ae4ed4707f6b3ad571450d3fd87db028650dbc
JulianaStadler <julianastadlerberger@gmail.com>
2025-09-09 14:08:16 -0300

    :(

--- Services/User/IUserService.cs
+++ Services/User/IUserService.cs
@@ -5,5 +5,9 @@ namespace ArtGallery.Services.Users;
 public interface IUserService
 {
     Task<User?> thisUserExist(Guid id);
+<<<<<<< HEAD
     Task<bool> thisUsernameIsInUse(string username);
+=======
+    Task<User> FindByLogin(string login);
+>>>>>>> 3d209cf (commit :))
 }
