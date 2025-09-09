08bae62d2abcdae24da7b3c2939101dffc3febba
JulianaStadler <julianastadlerberger@gmail.com>
2025-09-09 14:10:28 -0300

    fix errors

--- Services/User/IUserService.cs
+++ Services/User/IUserService.cs
@@ -5,9 +5,6 @@ namespace ArtGallery.Services.Users;
 public interface IUserService
 {
     Task<User?> thisUserExist(Guid id);
-<<<<<<< HEAD
     Task<bool> thisUsernameIsInUse(string username);
-=======
     Task<User> FindByLogin(string login);
->>>>>>> 3d209cf (commit :))
 }
--- Services/User/UserService.cs
+++ Services/User/UserService.cs
@@ -10,18 +10,17 @@ public class UserService(ArtGalleryDbContext ctx) : IUserService
         return await ctx.Users.FirstOrDefaultAsync(u => u.Id == id);
     }
 
-<<<<<<< HEAD
     public async Task<bool> thisUsernameIsInUse(string username)
     {
-        var thisUsernameIsInUse = await ctx.Users.FirstOrDefaultAsync(u => u.Name == username);    
-        return thisUsernameIsInUse != null ? true : false ;
-=======
+        var thisUsernameIsInUse = await ctx.Users.FirstOrDefaultAsync(u => u.Name == username);
+        return thisUsernameIsInUse != null ? true : false;
+    }
+    
     public async Task<User> FindByLogin(string login)
     {
         var user = await ctx.Users.FirstOrDefaultAsync(
             p => p.Name == login || p.Email == login
         );
         return user;
->>>>>>> 3d209cf (commit :))
     }
 }
